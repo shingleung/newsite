40c020480123f6c4df126f8ba524f1dcbe4e0a68
shingleung <143300464+shingleung@users.noreply.github.com>
2023-08-27 11:31:13 -0400

    test

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
-const text = new SplitType("#target", { types: "lines, words" });
 
-alert("hello");
+let elementsToSplit = $(".split-lines");
+let instancesOfSplit = [];
+// Split the text up
+function runSplit() {
+  elementsToSplit.each(function (index) {
+    let currentElement = $(this);
+    instancesOfSplit[index] = new SplitType(currentElement, {
+      types: "lines, words"
+    });
+  });
+  $(".line").each(function (index) {
+    $(this).append("<div class='line-mask'></div>");
+  });
+}
+runSplit();
+// Update on window resize
+let windowWidth = $(window).innerWidth();
+window.addEventListener("resize", function () {
+  if (windowWidth !== $(window).innerWidth()) {
+    windowWidth = $(window).innerWidth();
+    elementsToSplit.each(function (index) {
+      instancesOfSplit[index].revert();
+    });
+    runSplit();
+    createAnimation();
+  }
+});
